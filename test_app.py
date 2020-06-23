e533ab3163e63c8507fd43999d96ec03a61588da
Kevin Yaiko <kwyaiko@gmail.com>
2020-06-23 14:49:26 +0200

    Adds tests

--- test_app.py
+++ test_app.py
@@ -12,3 +12,32 @@ class BoggleTestCase(TestCase):
 
             self.assertEqual(res.status_code, 200)
             self.assertIn('<h1>Boggle Board</h1>', html)
+
+
+    def test_validate_word(self):
+        with app.test_client() as client:
+            board = client.get('/')
+            res = client.get('/check-word?word_value="hotdogspinachbunsaregross"')
+            # res.board = [["H", "O", "T", "T", "T"], 
+            #                      ["C", "A", "T", "T", "T"], 
+            #                      ["C", "A", "T", "T", "T"], 
+            #                      ["C", "A", "T", "T", "T"], 
+            #                      ["C", "A", "T", "T", "T"]]
+            # with client.session_transaction() as change:
+            #     change['board'] =[["H", "O", "T", "T", "T"], 
+            #                      ["C", "A", "T", "T", "T"], 
+            #                      ["C", "A", "T", "T", "T"], 
+            #                      ["C", "A", "T", "T", "T"], 
+            #                      ["C", "A", "T", "T", "T"]]
+            self.assertIn(b'not-word', res.data)
+    
+
+    def test_update_stats(self):
+        with app.test_client() as client:
+            res = client.get('/stats')
+            with client.session_transaction() as change:
+                change['games_played'] = 5
+
+            self.assertEqual(res.get_data(as_text=True), 5)
+
+
